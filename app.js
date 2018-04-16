efa2a8cb04a875333a13805da90d54256edcacba
omcl1302 <a20121302@pucp.pe>
2018-04-16 12:21:30 -0500

    first commit

--- app.js
+++ app.js
@@ -6,13 +6,10 @@ var cookieParser = require('cookie-parser');
 var morgan = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var testsRouter = require('./routes/tests');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(morgan('combined', { stream: winston.stream }));
 app.use(express.json());
@@ -21,7 +18,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/tests', testsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
