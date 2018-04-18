0f549fb2a952c5519eb4c2a051a7e3dbae93d47c
omcl1302 <a20121302@pucp.pe>
2018-04-18 14:14:22 -0500

    CORS enabled

--- app.js
+++ app.js
@@ -9,13 +9,19 @@ var indexRouter = require('./routes/index');
 var testsRouter = require('./routes/tests');
 
 var app = express();
-
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
 
 app.use(morgan('combined', { stream: winston.stream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(allowCrossDomain);
 
 app.use('/', indexRouter);
 app.use('/tests', testsRouter);
