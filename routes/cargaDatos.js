b9ad24b55db86554d747e1fc93d30471645ae121
omcl1302 <a20121302@pucp.pe>
2018-04-24 14:38:02 -0500

    added files to module cargaDatos

--- routes/cargaDatos.js
+++ routes/cargaDatos.js
@@ -4,8 +4,9 @@ const cargaController = require("../controller/cargaDatos/cargaPrincipal");
 
 /* GET home page. */
 router.post('/cargaDatos', async function(req, res) {
-    let carga = await cargaController.cargaPrincipal(req.body);
-    res.send(carga);
+    //let carga = await cargaController.cargaPrincipal(req.body);
+    console.log(req.body)
+    res.send(req.body);
 });
 
 module.exports = router;
