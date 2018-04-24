e497c36012a499ded9098aebcbc43eca4be36d6c
Oscar <Oscar@DESKTOP-44C66HF>
2018-04-24 18:07:35 -0500

    added method listaDocente

---
+++ controller/perfilDocente/listaDocentes.js
@@ -0,0 +1,30 @@
+const winston = require('../../config/winston');
+const dbCon = require('../../config/db');
+const Sequelize = require ('sequelize');
+const dbSpecs = dbCon.connect();
+const sequelize= new Sequelize(dbSpecs.db, dbSpecs.user, dbSpecs.password, {
+    host: dbSpecs.host,
+    dialect: dbSpecs.dialect,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+});
+
+async function listaDocente() {
+
+    try {
+        let response = sequelize.query('CALL listaDocentes ()');
+        winston.info("muestraCursosCiclo success");
+        return response;
+    } catch(e) {
+        winston.error("muestraCursosCiclo Failed: ",e);
+        return "error";
+    }
+}
+
+module.exports = {
+    listaDocente: listaDocente
+};
