cb688b4aee2b80eb824ce1b88101e46f06d03484
Oscar <Oscar@DESKTOP-44C66HF>
2018-04-28 17:24:01 -0500

    added code to cargaCurso

--- controller/cargaDatos/cargaCurso.js
+++ controller/cargaDatos/cargaCurso.js
@@ -13,14 +13,43 @@ const sequelize= new Sequelize(dbSpecs.db, dbSpecs.user, dbSpecs.password, {
     },
 });
 
-async function cargaCurso(preferencesObject) {
+async function cargaCurso(dataArray) {
+    let message = "";
 
     try {
-        let result = {};
+        await dataArray.map(async item => {
+            try{
+                let codigo = item[0];
+                let nombre = item[1];
+                let creditos = item[2];
+                let horasDictado = item[3];
+                let facultad = item[4];
+                let seccion = item[5];
+                let tipoCurso = item[6]
+                if (nombre === undefined || codigo === undefined || creditos === undefined|| horasDictado === undefined
+                    || facultad === undefined|| seccion === undefined|| tipoCurso === undefined){
+                    return message = "cargaCurso Failed undefined or empty columns";
+                }else{
+                    let esRepetido = await sequelize.query(`CALL verifica_docente_repetido (${dni})`);
+
+                    if (esRepetido[0] === undefined) {
+                        await sequelize.query(`CALL insert_docente ('${nombre}', '${codigo}', '${creditos}',
+                        ${horasDictado}, ${facultad}, '${seccion}', '${tipoCurso}')`);
+                    }
+                    return message = "cargaCurso success on execution";
+                }
+            }catch (e) {
+                winston.error("cargaCurso Failed: ",e);
+                message = "cargaCurso Failed";
+                return message;
+            }
+        });
         winston.info("cargaCurso success on execution");
-        return result;
+        return message;
     } catch(e) {
         winston.error("cargaCurso Failed: ",e);
+        message = "cargaCurso Failed";
+        return message;
     }
 }
 
