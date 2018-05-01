4491bc73760201eafb41a98a9adf0727e8a4f8c1
ingavalos <ingavalos_carlos@hotmail.com>
2018-05-01 16:05:29 -0500

    "All" support for Horas DEscarga

--- controller/perfilDocente/horasDescargaDocentes.js
+++ controller/perfilDocente/horasDescargaDocentes.js
@@ -14,13 +14,13 @@ const sequelize= new Sequelize(dbSpecs.db, dbSpecs.user, dbSpecs.password, {
 });
 
 
-function horasDescargaDetalle(preferencesObject,id_curso) {
+function horasDescargaDetalle(preferencesObject,id_curso,ciclo) {
     try {
         let result =  sequelize.query('CALL HORAS_DESCARGA_DETALLE(:codigo,:ciclo,:id_curso)',
             {
                 replacements: {
                     codigo: preferencesObject.codigo,
-                    ciclo: preferencesObject.ciclo,
+                    ciclo: ciclo,
                     id_curso: id_curso
                 }
             }
@@ -33,13 +33,31 @@ function horasDescargaDetalle(preferencesObject,id_curso) {
     }
 }
 
+
+
 function horasDescargaListar(preferencesObject) {
     try {
-        let result =  sequelize.query('CALL HORAS_DESCARGA_LISTAR(:codigo,:ciclo)',
+        let result =  sequelize.query('CALL HORAS_DESCARGA_LISTAR(:codigo)',
             {
                 replacements: {
-                    codigo: preferencesObject.codigo,
-                    ciclo: preferencesObject.ciclo
+                    codigo: preferencesObject.codigo
+                }
+            }
+        );
+        winston.info("horasDescargaListar succesful");
+        return result;
+    } catch (e) {
+        console.log(e);
+        winston.error("horasDescargaListar failed");
+    }
+}
+
+function horasDescargaListarAll(preferencesObject) {
+    try {
+        let result =  sequelize.query('CALL HORAS_DESCARGA_LISTAR(:codigo)',
+            {
+                replacements: {
+                    codigo: preferencesObject.codigo
                 }
             }
         );
@@ -52,8 +70,31 @@ function horasDescargaListar(preferencesObject) {
 }
 
 
+async function hDescAll(preferencesObject){
+    try {
 
-async function horasDescarga(preferencesObject) {
+        let jsonHorasDescargaListar = await horasDescargaListarAll(preferencesObject);
+
+        let jsonHorasDescargaDetalle = Promise.all(jsonHorasDescargaListar.map(async item => {
+            let innerPart = {};
+            innerPart.nombre = item.nombre;
+            innerPart.codigo = item.codigo;
+            //innerPart.hDictadas = item.hDictadas;
+            innerPart.hDescargaTotal = item.hDescarga;
+            let listaSemanal = await horasDescargaDetalle(preferencesObject,item.id_curso,item.ciclo);
+            innerPart.semana = listaSemanal;
+            return innerPart;
+        }));
+
+        return jsonHorasDescargaDetalle;
+
+    } catch (e){
+        console.log(e);
+        winston.error("listaEncuestas failed");
+    }
+}
+
+async function hDescCiclo(preferencesObject){
     try {
 
         let jsonHorasDescargaListar = await horasDescargaListar(preferencesObject);
@@ -64,13 +105,29 @@ async function horasDescarga(preferencesObject) {
             innerPart.codigo = item.codigo;
             //innerPart.hDictadas = item.hDictadas;
             innerPart.hDescargaTotal = item.hDescarga;
-            let listaSemanal = await horasDescargaDetalle(preferencesObject,item.id_curso);
+            let listaSemanal = await horasDescargaDetalle(preferencesObject,item.id_curso,item.ciclo);
             innerPart.semana = listaSemanal;
             return innerPart;
         }));
 
         return jsonHorasDescargaDetalle;
 
+    } catch (e){
+        console.log(e);
+        winston.error("listaEncuestas failed");
+    }
+}
+
+async function horasDescarga(preferencesObject) {
+    try{
+        let c = preferencesObject.ciclo;
+        console.log(c);
+
+        if (c == "all"){
+            return await hDescAll(preferencesObject);
+        }else{
+            return await hDescCiclo(preferencesObject);
+        }
     } catch (e){
         console.log(e);
         winston.error("listaEncuestas failed");
