f138c1b83038759d98e79f2c88d39ed66237e77d
ingavalos <ingavalos_carlos@hotmail.com>
2018-05-01 16:00:31 -0500

    Now Encuestas_Docente support "all"

--- controller/perfilDocente/encuestasDocente.js
+++ controller/perfilDocente/encuestasDocente.js
@@ -15,10 +15,28 @@ const sequelize= new Sequelize(dbSpecs.db, dbSpecs.user, dbSpecs.password, {
 
 function queryListaEncuestas(preferencesObject) {
     try {
+        let result =  sequelize.query('CALL ENCUESTA_LISTAR_CICLO(:codigo,:ciclo)',
+            {
+                replacements: {
+                    codigo: preferencesObject.codigo, // codigo del profesor
+                    ciclo: preferencesObject.ciclo
+                }
+            }
+        );
+        winston.info("queryListaEncuestas succesful");
+        return result;
+    } catch (e) {
+        console.log(e);
+        winston.error("queryListaEncuestas failed");
+    }
+}
+
+function queryListaEncuestasAll(preferencesObject) {
+    try {
         let result =  sequelize.query('CALL ENCUESTA_LISTAR(:codigo)',
             {
                 replacements: {
-                    codigo: preferencesObject.codigo // codigo del profesor
+                    codigo: preferencesObject.codigo // codigo del profeso
                 }
             }
         );
@@ -31,13 +49,13 @@ function queryListaEncuestas(preferencesObject) {
 }
 
 
-function queryListaComentarios(preferencesObject,id_curso) {
+function queryListaComentarios(preferencesObject,id_curso,ciclo) {
     try {
         let result =  sequelize.query('CALL ENCUESTA_COMENTARIOS(:codigo,:ciclo,:id_curso)',
             {
                 replacements: {
                     codigo: preferencesObject.codigo,
-                    ciclo: preferencesObject.ciclo,
+                    ciclo: ciclo,
                     id_curso: id_curso
                 }
             }
@@ -50,12 +68,11 @@ function queryListaComentarios(preferencesObject,id_curso) {
     }
 }
 
-
-
-async function listaEncuestas(preferencesObject) {
+async function listaEnAll(preferencesObject){
     try {
+        console.log("ALL");
+        let jsonEncuestas = await queryListaEncuestasAll(preferencesObject);
 
-        let jsonEncuestas = await queryListaEncuestas(preferencesObject);
 
         let jsonEncuestasComentarios = Promise.all(jsonEncuestas.map(async item => {
             let innerPart = {};
@@ -64,13 +81,59 @@ async function listaEncuestas(preferencesObject) {
             innerPart.horario = item.horario;
             innerPart.porcentaje = item.porcentaje;
             innerPart.puntaje = item.puntaje;
-            let listaComentarios = await queryListaComentarios(preferencesObject,item.id_curso);
+            let listaComentarios = await queryListaComentarios(preferencesObject,item.id_curso,item.ciclo);
             innerPart.comentarios = listaComentarios;
             return innerPart;
         }));
 
         return jsonEncuestasComentarios;
 
+
+
+    } catch (e){
+        console.log(e);
+        winston.error("listaEncuestas failed");
+    }
+}
+
+async function listaEnxCiclo(preferencesObject){
+    try {
+        console.log("XCICLO");
+        let jsonEncuestas = await queryListaEncuestas(preferencesObject);
+
+
+        let jsonEncuestasComentarios = Promise.all(jsonEncuestas.map(async item => {
+            let innerPart = {};
+            innerPart.id = item.id;
+            innerPart.curso = item.curso;
+            innerPart.horario = item.horario;
+            innerPart.porcentaje = item.porcentaje;
+            innerPart.puntaje = item.puntaje;
+            let listaComentarios = await queryListaComentarios(preferencesObject,item.id_curso,item.ciclo);
+            innerPart.comentarios = listaComentarios;
+            return innerPart;
+        }));
+
+        return jsonEncuestasComentarios;
+
+
+
+    } catch (e){
+        console.log(e);
+        winston.error("listaEncuestas failed");
+    }
+}
+
+async function listaEncuestas(preferencesObject) {
+    try{
+        let c = preferencesObject.ciclo;
+        console.log(c);
+
+        if (c == "all"){
+            return await listaEnAll(preferencesObject);
+        }else{
+            return await listaEnxCiclo(preferencesObject);
+        }
     } catch (e){
         console.log(e);
         winston.error("listaEncuestas failed");
