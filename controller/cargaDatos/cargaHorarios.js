2ab848f8da746614a336f1226c71cc01aaee53bb
omcl1302 <a20121302@pucp.pe>
2018-05-02 17:37:21 -0500

    added method cargaHorarios

--- controller/cargaDatos/cargaHorarios.js
+++ controller/cargaDatos/cargaHorarios.js
@@ -16,6 +16,28 @@ const sequelize= new Sequelize(dbSpecs.db, dbSpecs.user, dbSpecs.password, {
 async function cargaHorarios(preferencesObject) {
 
     try {
+        await dataArray.map(async item => {
+            try{
+                let codigoProf = item[0];
+                let codigoCurso = item[1];
+                let ciclo = item[2];
+                let codigoHorario = item[3];
+
+                if (codigoProf === undefined || codigoCurso === undefined || ciclo === undefined
+                    || codigoHorario === undefined ){
+                    return message = "cargaHorarios Failed undefined or empty columns";
+                }else{
+                    await sequelize.query(`CALL insert_horario (${codigoProf}, '${codigoCurso}', '${ciclo}',
+                    '${codigoHorario}')`);
+
+                    return message = "cargaHorarios  success on execution";
+                }
+            }catch (e) {
+                winston.error("cargaHorarios Failed: ",e);
+                message = "cargaHorarios Failed";
+                return message;
+            }
+        });
         let result = {};
         winston.info("cargaHorarios success on execution");
         return result;
