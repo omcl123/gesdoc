a775b844143170bb7755c1e0bd1b593bac4bed17
ingavalos <ingavalos_carlos@hotmail.com>
2018-05-19 19:55:56 -0500

    little fixes in routes convocatorias

--- routes/convocatoria.js
+++ routes/convocatoria.js
@@ -9,7 +9,7 @@ const postulanteController = require('../controller/convocatoria/postulante.js')
 //Lista convocatorias
 router.get('/convocatoria/lista', async function(req, res) {
     let jsonBlock={};
-    jsonBlock.ayudas = await convocatoriaController.listaConvocatoria(req.query);
+    jsonBlock.convocatorias= await convocatoriaController.listaConvocatoria(req.query);
 
     res.send(jsonBlock);
 
@@ -19,11 +19,12 @@ router.get('/convocatoria/lista', async function(req, res) {
 //Detalle convocatoria
 router.get('/convocatoria/detalle', async function(req, res) {
     let jsonBlock={};
-    jsonBlock.ayudas = await convocatoriaController.detalleConvocatoria(req.query);
+    jsonBlock = await convocatoriaController.detalleConvocatoria(req.query);
 
     res.send(jsonBlock);
 
 });
+
 
 //Listar Postulante
 router.get('/convocatoria/postulante/listar', async function (req,res){
@@ -31,8 +32,6 @@ router.get('/convocatoria/postulante/listar', async function (req,res){
     jsonBlock.postulante=await postulanteController.listarPostulante(req.query);
     res.send(jsonBlock);
 });
-
-
 
 
 
