193621e4ffeb155b4bddb78d59072d61475f41bd
Oscar <Oscar@DESKTOP-44C66HF>
2018-06-03 18:50:13 -0500

    if person already exists just add user

--- routes/tests.js
+++ routes/tests.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const multer = require('multer');
 const router = express.Router();
 const VerifyToken = require('../auth/VerifyToken');
+
+const UPLOAD_PATH = 'home/inf245/files/test/';
+const upload = multer({ dest: `${UPLOAD_PATH}/` });
+const type = upload.single('file');
 /* GET users listing. */
 router.get('/',VerifyToken,function(req, res, next) {
 	let jsonBlock = {};
@@ -8,5 +13,16 @@ router.get('/',VerifyToken,function(req, res, next) {
 	console.log(req.user);
 	res.send(jsonBlock);
 });
+
+router.post('/upload', type, async (req, res) => {
+    try {
+        console.log(req.body);
+        console.log(req.file);
+
+        res.send(req.file);
+    } catch (err) {
+        res.sendStatus(400);
+    }
+})
 
 module.exports = router;
