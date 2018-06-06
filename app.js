e1d3b9e6d25ce3205ed6b3f0d4c111d5526cbe57
Oscar <Oscar@DESKTOP-44C66HF>
2018-06-05 20:36:34 -0500

    added test download file

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 const allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
     next();
 };
 
