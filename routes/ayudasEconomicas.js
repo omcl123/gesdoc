389fe871a819666805ad0ce74306d585c38b0e7f
omcl1302 <a20121302@pucp.pe>
2018-06-06 14:23:21 -0500

    bug fixes

--- routes/ayudasEconomicas.js
+++ routes/ayudasEconomicas.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const VerifyToken = require('../auth/VerifyToken');
 
 const ayudasEconomicasAsistenteController = require('../controller/ayudasEconomicas/ayudasEconomicasAsistente.js'); //carlos
 const ayudasEconomicasJefeController = require('../controller/ayudasEconomicas/ayudasEconomicasJefe.js'); //moises
@@ -9,48 +9,48 @@ const ayudasEconomicasController = require('../controller/ayudasEconomicas/ayuda
 
 
 
-router.get('/ayudasEconomicas/listar',async function (req,res){
+router.get('/ayudasEconomicas/listar',VerifyToken,async function (req,res){
     let jsonBlock={};
     jsonBlock.ayudaEconomica = await ayudasEconomicasJefeController.devuelveAyudasEconomicas(req.query);
     res.send(jsonBlock);
 });
-router.get('/ayudasEconomicas/devuelveJustificacion',async function (req,res){
+router.get('/ayudasEconomicas/devuelveJustificacion',VerifyToken,async function (req,res){
     let jsonBlock={};
     jsonBlock.ayudaEconomica = await ayudasEconomicasJefeController.devuelveAyudaEconomicaJustificacion(req.query);
     res.send(jsonBlock);
 });
-router.get('/ayudasEconomicas/filtrar',async function (req,res){
+router.get('/ayudasEconomicas/filtrar',VerifyToken,async function (req,res){
     let jsonBlock={};
     jsonBlock.ayudaEconomica = await ayudasEconomicasJefeController.devuelveAyudasEconomicasFiltro(req.query);
     res.send(jsonBlock);
 });
-router.get('/ayudasEconomicas/detallar',async function (req,res){
+router.get('/ayudasEconomicas/detallar',VerifyToken,async function (req,res){
     let jsonBlock={};
     jsonBlock.ayudaEconomica = await ayudasEconomicasJefeController.devuelveDetalleAyudaEconomica(req.query);
     res.send(jsonBlock);
 });
 
-router.put('/ayudasEconomicas/modificar',async function (req,res){
+router.put('/ayudasEconomicas/modificar',VerifyToken,async function (req,res){
     let jsonBlock={};
     jsonBlock.mensaje = await ayudasEconomicasJefeController.modificarAyudaEconomica(req.body);
     res.send(jsonBlock);
 });
 //registrar ayudaEconomica
-router.post('/ayudasEconomicas/registrar', async function (req,res) {
+router.post('/ayudasEconomicas/registrar',VerifyToken, async function (req,res) {
     let jsonBlock = {}
     jsonBlock = await ayudasEconomicasAsistenteController.registrarAyudaEconomica(req.body);
     res.send(jsonBlock);
 });
 
 //registrar gasto
-router.post('/ayudasEconomicas/DocumentoGasto/registrar', async function (req,res) {
+router.post('/ayudasEconomicas/DocumentoGasto/registrar',VerifyToken, async function (req,res) {
     let jsonBlock = {}
     jsonBlock = await ayudasEconomicasAsistenteController.registrarDocumentoGasto(req.body);
     res.send(jsonBlock);
 });
 
 //listar motivo
-router.get('/ayudasEconomicas/motivos',async function (req,res){
+router.get('/ayudasEconomicas/motivos',VerifyToken,async function (req,res){
     let jsonBlock={};
     jsonBlock.motivos = await ayudasEconomicasController.listarMotivos(req.query);
     res.send(jsonBlock);
@@ -58,7 +58,7 @@ router.get('/ayudasEconomicas/motivos',async function (req,res){
 
 
 //modificar ayuda economica
-router.put('/ayudasEconomicasAsistente/modificar',async function (req,res){
+router.put('/ayudasEconomicasAsistente/modificar',VerifyToken,async function (req,res){
     let jsonRes={}
     jsonRes.mensaje=await ayudasEconomicasAsistenteController.modificarAyudaEconomica(req.body);
     res.send(jsonRes);
@@ -66,7 +66,7 @@ router.put('/ayudasEconomicasAsistente/modificar',async function (req,res){
 });
 
 //eliminar ayuda economica
-router.delete('/ayudasEconomicasAsistente/rechazar',async function (req,res){
+router.delete('/ayudasEconomicasAsistente/rechazar',VerifyToken,async function (req,res){
     let jsonRes={}
     console.log(JSON.stringify(req.body));
     jsonRes.mensaje=await ayudasEconomicasAsistenteController.rechazaAyudaEconomica(req.body);
@@ -74,7 +74,7 @@ router.delete('/ayudasEconomicasAsistente/rechazar',async function (req,res){
 });
 
 //eliminar eliminar justificacion
-router.delete('/ayudasEconomicasAsistente/DocumentoGasto/eliminar',async function (req,res){
+router.delete('/ayudasEconomicasAsistente/DocumentoGasto/eliminar',VerifyToken,async function (req,res){
     let jsonRes={}
     console.log(JSON.stringify(req.body));
     jsonRes.mensaje=await ayudasEconomicasAsistenteController.eliminarDocumentoGasto(req.body);
