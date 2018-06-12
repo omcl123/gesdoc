2d8117b6b746b55c22c2f5d7a94b1e3e927e3ed9
omcl1302 <a20121302@pucp.pe>
2018-06-12 17:10:17 -0500

    bug fixes

---
+++ routes/visionGeneralRoutes.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const router = express.Router();
+const dashboardController = require('../controller/vistaGeneral/vistaGeneral');
+const VerifyToken = require('../auth/VerifyToken');
+
+//departamento
+
+router.get('/docentesTipoDepartamento',VerifyToken,async function(req, res) {
+    let jsonblock;
+    let user = req.body.verifiedUser;
+    if (user.id_cargo=== 3 ||user.id_cargo === 4  ) {// 2 secion , 3 jefe, 4 asis dep , 5 asis secc;
+        jsonblock = await dashboardController.docentesTipoDepartamento(req.query,user.unidad);
+        res.send(jsonblock);
+    }else {
+        return res.status(500).send('Invalid Permits');
+    }
+});
+
+router.get('/actividadesTipoDepartamento',VerifyToken,async function(req, res) {
+    let jsonblock;
+    let user = req.body.verifiedUser;
+    if (user.id_cargo=== 3 || user.id_cargo === 4  ) {// 2 secion , 3 jefe, 4 asis dep , 5 asis secc;
+        jsonblock = await dashboardController.actividadesTipoDepartamento(req.query,user.unidad);
+        res.send(jsonblock);
+    }else {
+        return res.status(500).send('Invalid Permits');
+    }
+});
+
+router.get('/apoyoEconomicoEstadoDepartamento',VerifyToken,async function(req, res) {
+    let jsonblock;
+    let user = req.body.verifiedUser;
+    if (user.id_cargo=== 3 || user.id_cargo === 4  ) {// 2 secion , 3 jefe, 4 asis dep , 5 asis secc;
+        jsonblock = await dashboardController.apoyoEconomicoEstadoDepartamento(req.query,user.unidad);
+        res.send(jsonblock);
+    }else {
+        return res.status(500).send('Invalid Permits');
+    }
+});
+
+router.get('/convocatoriaEstadoDepartamento',VerifyToken,async function(req, res) {
+    let jsonblock;
+    let user = req.body.verifiedUser;
+    if (user.id_cargo=== 3 || user.id_cargo === 4 || user.id_cargo === 2 ) {// 2 secion , 3 jefe, 4 asis dep , 5 asis secc;
+        jsonblock = await dashboardController.convocatoriaEstadoDepartamento(req.query,user.unidad);
+        res.send(jsonblock);
+    }else {
+        return res.status(500).send('Invalid Permits');
+    }
+});
+
+//seccion
+
+router.get('/docentesTipoSeccion',VerifyToken,async function(req, res) {
+    let jsonblock;
+    let user = req.body.verifiedUser;
+    if (user.id_cargo=== 3 || user.id_cargo === 4 || user.id_cargo === 2 ) {// 2 secion , 3 jefe, 4 asis dep , 5 asis secc;
+        jsonblock = await dashboardController.docentesTipoSeccion(req.query);
+        res.send(jsonblock);
+    }else {
+        return res.status(500).send('Invalid Permits');
+    }
+});
+
+router.get('/actividadesTipoSeccion',VerifyToken,async function(req, res) {
+    let jsonblock;
+    let user = req.body.verifiedUser;
+    if (user.id_cargo=== 3 || user.id_cargo === 4 || user.id_cargo === 2 ) {// 2 secion , 3 jefe, 4 asis dep , 5 asis secc;
+        jsonblock = await dashboardController.actividadesTipoSeccion(req.query);
+        res.send(jsonblock);
+    }else {
+        return res.status(500).send('Invalid Permits');
+    }
+});
+
+router.get('/apoyoEconomicoEstadoSeccion',VerifyToken,async function(req, res) {
+    let jsonblock;
+    let user = req.body.verifiedUser;
+    if (user.id_cargo=== 3 || user.id_cargo === 4 || user.id_cargo === 2 ) {// 2 secion , 3 jefe, 4 asis dep , 5 asis secc;
+        jsonblock = await dashboardController.apoyoEconomicoEstadoSeccion(req.query);
+        res.send(jsonblock);
+    }else {
+        return res.status(500).send('Invalid Permits');
+    }
+});
+
+router.get('/convocatoriaEstadoSeccion',VerifyToken,async function(req, res) {
+    let jsonblock;
+    let user = req.body.verifiedUser;
+    if (user.id_cargo=== 3 || user.id_cargo === 4 || user.id_cargo === 2 ) {// 2 secion , 3 jefe, 4 asis dep , 5 asis secc;
+        jsonblock = await dashboardController.convocatoriaEstadoSeccion(req.query);
+        res.send(jsonblock);
+    }else {
+        return res.status(500).send('Invalid Permits');
+    }
+});
+
+
+module.exports = router;
