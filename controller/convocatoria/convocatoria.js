46283e4fc4199b02493479110ddc59f1e67472de
Carlos Eduardo <ingavalos_carlos@hotmail.com>
2018-06-22 00:45:26 -0500

    fixes

--- controller/convocatoria/convocatoria.js
+++ controller/convocatoria/convocatoria.js
@@ -213,7 +213,6 @@ async function insertaConvocatoria(preferencesObject){
     let fecha_f;
     let nombre;
     let seccion;
-    let descripcion;
     let requiere_investigacion = 0;
     let requiere_experiencia = 0;
     let requiere_docencia_cargo = 0;
@@ -270,13 +269,6 @@ async function insertaConvocatoria(preferencesObject){
         seccion = null;
     }
 
-    if (preferencesObject.descripcion != null) {
-        console.log("descripcion NO es nulo");
-        descripcion = preferencesObject.descripcion;
-    } else {
-        console.log("descripcion es nulo");
-        descripcion = null;
-    }
 
     if (preferencesObject.grados_academicos != null) {
         console.log("grados_academicos NO es nulo");
@@ -351,13 +343,12 @@ async function insertaConvocatoria(preferencesObject){
         console.log("investigacion es nulo");
     }
 
-    await sequelize.query('CALL insertaConvocatoria(:nombre,:seccion,:descipcion,:fecha_inicio,:fecha_fin,:requiere_investigacion,:requiere_experiencia,:requiere_docencia_cargo,:requiere_docencia_asesoria,:requiere_docencia_premio,:requiere_grado_titulo,:requiere_grado_maestria,:requiere_grado_doctorado,:requiere_grado_diplomatura,:peso_investigacion,:peso_experiencia,:peso_docencia_cargo,:peso_docencia_asesoria,:peso_docencia_premio,:peso_grado_titulo,:peso_grado_maestria,:peso_grado_doctorado,:peso_grado_diplomatura)',
+    await sequelize.query('CALL insertaConvocatoria(:nombre,:seccion,:fecha_inicio,:fecha_fin,:requiere_investigacion,:requiere_experiencia,:requiere_docencia_cargo,:requiere_docencia_asesoria,:requiere_docencia_premio,:requiere_grado_titulo,:requiere_grado_maestria,:requiere_grado_doctorado,:requiere_grado_diplomatura,:peso_investigacion,:peso_experiencia,:peso_docencia_cargo,:peso_docencia_asesoria,:peso_docencia_premio,:peso_grado_titulo,:peso_grado_maestria,:peso_grado_doctorado,:peso_grado_diplomatura)',
         {
 
             replacements: {
                 nombre: nombre,
                 seccion:seccion,
-                descipcion: descripcion,
                 fecha_inicio: fecha_i,
                 fecha_fin: fecha_f,
                 requiere_investigacion: requiere_investigacion,
